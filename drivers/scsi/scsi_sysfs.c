e58d242284e4f15caee986b42abe99934cf75ef8
Russell King <rmk+kernel@arm.linux.org.uk>
2013-04-27 23:31:10 +0100

    ARM: Allow selection HZ values
    Allow users to configure the Hz rate on implementations which do not have
    a fixed clock rate.
    
    Whenever HZ_FIXED is not set through one of its "default" settings, the
    user is allowed to select a tick rate from 100, 200, 250, 300, 500Hz
    and 1kHz.  This is slightly more choice than with the generic HZ
    selection in kernel/Kconfig.hz (which only does 100, 250, 300Hz and
    1kHz).  The reason for including 200Hz is that a greater number of other
    platforms want that via the fixed rate, and 500Hz just seemed to be a
    better middle value than 300Hz (which is of course very close to 250.)
    
    Change-Id: I81b410a09dcbecbd9a82c3957472323aeaf2989e
    Signed-off-by: Russell King <rmk+kernel@arm.linux.org.uk>
    
    fix

--- drivers/scsi/scsi_sysfs.c
+++ drivers/scsi/scsi_sysfs.c
@@ -972,19 +972,11 @@ void __scsi_remove_device(struct scsi_device *sdev)
 		sdev->host->hostt->slave_destroy(sdev);
 	transport_destroy_device(dev);
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
 	/* cause the request function to reject all I/O requests */
 	sdev->request_queue->queuedata = NULL;
 
 	/* Freeing the queue signals to block that we're done */
 	scsi_free_queue(sdev->request_queue);
->>>>>>> parent of 9413ee2... [SCSI] Fix device removal NULL pointer dereference
-=======
-	/* Freeing the queue signals to block that we're done */
-	blk_cleanup_queue(sdev->request_queue);
->>>>>>> parent of 9cfacbb... [SCSI] Stop accepting SCSI requests before removing a device
 	put_device(dev);
 }
 
